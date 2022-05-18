2fd97bc71c36cb17667b014c8b0604e7b8acf7dd
borja <brochebaquero@gmail.com>
2022-05-18 10:59:50 +0200

    subo clase 2

---
+++ Password2.java
@@ -0,0 +1,40 @@
+
+public class Password2 {
+
+
+	// esto lo copias y lo pegas en el main joseto Password2.getPassword(Password2.MINUSCULAS+Password2.MAYUSCULAS+Password2.ESPECIALES,10);
+	
+	public static String NUMEROS = "0123456789";
+	 
+	public static String MAYUSCULAS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+ 
+	public static String MINUSCULAS = "abcdefghijklmnopqrstuvwxyz";
+ 
+	public static String ESPECIALES = "Ò—";
+ 
+	
+	public static String getPinNumber() {
+		return getPassword(NUMEROS, 4);
+	}
+ 
+	public static String getPassword() {
+		return getPassword(8);
+	}
+ 
+	public static String getPassword(int length) {
+		return getPassword(NUMEROS + MAYUSCULAS + MINUSCULAS, length);
+	}
+ 
+	public static String getPassword(String key, int length) {
+		String pswd = "";
+ 
+		for (int i = 0; i < length; i++) {
+			pswd+=(key.charAt((int)(Math.random() * key.length())));
+		}
+ 
+		return pswd;
+		
+	}
+	
+		
+}
