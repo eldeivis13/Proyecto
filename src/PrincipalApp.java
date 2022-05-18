356f127e70b9059bc206ce2d1d1e480a78d33796
joose <joose@DESKTOP-JUQV5MV>
2022-05-18 12:48:20 +0200

    subo main

---
+++ src/PrincipalApp.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class PrincipalApp {
+	 
+	static Scanner sc= new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		System.out.println("# ----- GENERADOR DE CONTRASEÑA ALEATORIA ----- #");
+		System.out.println("Escoge la longitud de tu contraseña: ");
+		String longc = sc.next();
+	}
+}
